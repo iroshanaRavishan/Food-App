896d6abb3a489b1652657d49df5ea83a2ead8528
IroshanaRavishan <bandarairoshana@gamil.com>
2024-08-13 23:24:38 +0530

    added styles to food details page, added food details css file, brought ingredient items to a separate comp,

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import styles from "./fooddetails.module.css"
+import ItemList from "./ItemList";
 
 export default function FoodDetails({foodId}){
     const [isLoading, setIsLoading] = useState(true)
@@ -17,29 +19,29 @@ export default function FoodDetails({foodId}){
         fetchFood()
     }, [foodId])
 
-    return <div>
+    return (
        <div>
-            <div>
-                <h1>{food.title}</h1>
-                <img src={food.image} alt={food.image} />
-            
-                <div>
-                    <span>
-                        <strong> 🕑 {food.readyInMinutes} Minutes</strong>
-                    </span>
-                    <span>{food.vegetarian? "🥕 Vegi": "🍗 Non-Vegi"}</span>
-                    <span>👪 Serves {food.servings}</span>
-                    <span>{food.vegan? "🐄 Vegan":""}</span>
+            <div className={styles.recipeCard}>
+                <h1 className={styles.recipeName}>{food.title}</h1>
+                <img className={styles.recipeImage} src={food.image} alt={food.image} />
+                <div className={styles.recipeDetails}>
+                    <span><strong> 🕑 {food.readyInMinutes} Minutes</strong></span>
+                    <span><strong>👪 Serves {food.servings}</strong></span>
+                    <span><strong>{food.vegetarian? "🥕 Vegi": "🍗 Non-Vegi"}</strong></span>
+                    <span><strong>{food.vegan? "🐄 Vegan":""}</strong></span>
                 </div>
                 <div>
-                    💲{food.pricePerServing / 100} Per serving
+                <strong>💲{food.pricePerServing / 100} Per serving</strong>
                 </div>
-            </div>
-            <div>
-                
+                <h2>Ingredients</h2>
+                    <ItemList food={food} isLoading={isLoading}/>
                 <h2>Instructions</h2>
-                {isLoading ? (<p>Loading...</p>) : (food.analyzedInstructions[0].steps.map((steps)=>( <li>{steps.step}</li>)))}
+                <div className={styles.recipeinstructions}>
+                    <ol>
+                        {isLoading ? (<p>Loading...</p>) : (food.analyzedInstructions[0].steps.map((steps)=>( <li>{steps.step}</li>)))}
+                    </ol>
+                </div>
             </div>
         </div>
-    </div>
+    )
 }
