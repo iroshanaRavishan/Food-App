64ce1ec02a33c70937ef6cd8441b459ca2e88079
IroshanaRavishan <bandarairoshana@gamil.com>
2024-08-31 22:55:36 +0530

    installing new packages and updating them

--- foodapp.API/foodapp.API/obj/Debug/net8.0/foodapp.API.AssemblyInfo.cs
+++ foodapp.API/foodapp.API/obj/Debug/net8.0/foodapp.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("foodapp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc7714bafcef02d352a64577fac7cf0fc1ccf2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe694b83be796672279fdbd14fffbecc279fd9c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("foodapp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("foodapp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
