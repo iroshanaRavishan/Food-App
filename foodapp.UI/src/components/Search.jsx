1c76899aa9c56c559428742224e20ab157f21ba8
IroshanaRavishan <bandarairoshana@gamil.com>
2024-08-31 13:21:42 +0530

    moved the existing reactJS project from the original place

---
+++ foodapp.UI/src/components/Search.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import styles from "./search.module.css"
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch"
+const API_KEY = "fff68eceefed475780ed9e1c5a517b59"
+
+export default function({foodData, setFoodData}) {
+    const [query, setQuery] = useState("Pizza");
+    useEffect(()=> {
+        async function fetchFood(){
+            const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+            const data = await res.json()
+            console.log("The Response: ", data.results);
+            setFoodData(data.results);
+
+        }
+        fetchFood()
+    }, [query])
+
+    return <div className={styles.searchContainter}>
+        <input className={styles.input} onChange={(e)=> setQuery(e.target.value)} type="text" value={query}/>
+    </div>
+}
