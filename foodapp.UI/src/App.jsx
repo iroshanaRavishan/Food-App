d3b0fdcefd4e197515ea8df0ee786fa83d232865
IroshanaRavishan <bandarairoshana@gamil.com>
2024-09-01 13:55:12 +0530

    modified the App.jsx to use protect router and moved the searcha and comp inside the Container Comp to home

--- foodapp.UI/src/App.jsx
+++ foodapp.UI/src/App.jsx
@@ -1,28 +1,47 @@
 import { useState } from "react"
-import Search from "./components/Search"
-import FoodList from "./components/FoodList"
 import Nav from "./components/Nav"
+import Search from './components/Search'
 import "./app.css"
-import Container from "./components/Container"
-import InnerContainer from "./components/InnerContainer"
-import FoodDetails from "./components/FoodDetails"
+
+import { Route, Router, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import ProtectedRouter from "./ProtectedRouter"
+import UserRegister from "./components/UserRegister"
+import UserLogin from "./components/UserLogin"
+import Home from "./components/Home"
 
 function App() {
   const [foodData, setFoodData] = useState([]);
   const [foodId, setFoodId] = useState("656329");
 
+  const router = createBrowserRouter (
+    createRoutesFromElements(
+      <Route path="/">
+        <Route element={ProtectedRouter}>
+          <Route path="/" element={ <Search /> } />
+          <Route path="/" element={ <Home foodData={foodData} setFoodData={setFoodData} setFoodId={setFoodId} foodId={foodId} /> } />
+          {/* put the path to protect the routing */}
+        </Route>
+        <Route path="/login" element={ <UserLogin /> } />
+        <Route path="/register" element={ <UserRegister /> } />
+        <Route path="*" element={
+          <div>
+            <header>
+              <h1>Not Found!</h1> 
+            </header>
+            <p>
+              <a href="/">Back to Home</a>
+            </p>
+          </div>
+        } />
+      </Route>
+    )
+  )
+
+  const isLogged = localStorage.getItem("user");
+  
   return (
     <div className="App">
-      <Nav />
-      <Search foodData={foodData} setFoodData={setFoodData} />
-      <Container>
-        <InnerContainer>
-          <FoodList setFoodId={setFoodId} foodData={foodData}/>
-        </InnerContainer>
-        <InnerContainer>
-          <FoodDetails foodId={foodId} />
-        </InnerContainer>
-      </Container>
+      <Nav isLogged={isLogged}/>
     </div>
   )
 }
