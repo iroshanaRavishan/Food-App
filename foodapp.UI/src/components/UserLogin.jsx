3e4588d3a87cd96b54f8846441b0cfabcb0e0b26
IroshanaRavishan <bandarairoshana@gamil.com>
2024-09-10 21:09:43 +0530

    changed the way of collecting data from the login form

--- foodapp.UI/src/components/UserLogin.jsx
+++ foodapp.UI/src/components/UserLogin.jsx
@@ -1,8 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 export default function UserLogin() {
   const navigate = useNavigate(); // Hook to programmatically navigate
+  
+  const [formData, setFormData] = useState({
+    Email:'',
+    Password:'',
+    Remember:false
+  });
+
+  const [errors, setErrors] = useState({});
   
   // here, it does not ask an already logged in user to the login over and over again
   useEffect(()=>{
@@ -12,46 +20,60 @@ export default function UserLogin() {
     }
   }, []);
 
+  function handleChange(e){
+    const {name, value} = e.target;
+    setFormData({
+      ...formData, [name] : value
+    });
+  }
+
   async function loginHandler(e){
-
-    e.preventDefault();
-    const form_ = e.target, submitter = document.querySelector("input.login");
-    const formData = new FormData(form_, submitter), dataToSend = {};
-
-    for(const [key, value] of formData) {
-      dataToSend[key] = value;
+    e.preventDefault();    
+    const validationErrors = {};
+    if(!formData.Email.trim()){
+      validationErrors.Email = "Email is required!";
+    } else if(/\S+@\S+\.S+/.test(formData.Email)){
+      validationErrors.Email = "Email is not valid!";
     }
 
-    if (dataToSend.Remember === 'on') {
-      dataToSend.Remember = true;
+    if(!formData.Password.trim()) {
+      validationErrors.Password = "Password is required!";
     }
 
-    console.log("login data before send", dataToSend);
-    const response = await fetch("https://localhost:7181/api/Auth/login", {
-      method: "POST",
-      credentials: "include",
-      body: JSON.stringify(dataToSend),
-      headers: {
-        "content-type": "Application/json",
-        "Accept": "application/json"
+    if (formData.Remember === 'on') {
+      formData.Remember = true;
+    }
+
+    setErrors(validationErrors);
+
+    console.log("login data before send", formData);
+    if (Object.keys(validationErrors).length === 0) {
+      const response = await fetch("https://localhost:7181/api/Auth/login", {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(formData),
+        headers: {
+          "content-type": "Application/json",
+          "Accept": "application/json"
+        }
+      });
+
+      const data = await response.json();
+      const messageElement = document.querySelector(".message");
+  
+      if(data.message) {
+        messageElement.innerHTML = data.message;
       }
-    })
-
-    const data = await response.json();
-    const messageElement = document.querySelector(".message");
-
-    if(data.message) {
-      messageElement.innerHTML = data.message;
-    }
-    else {
-      messageElement.innerHTML = "Something went wrong! Please try agin later.";
-    }
-
-    console.log("login status: ", data);
-
-    if(response.ok) {
-      localStorage.setItem("user", dataToSend.Email);
-      navigate('/home');
+      else {
+        messageElement.innerHTML = "Something went wrong! Please try agin later.";
+      }
+  
+      console.log("login status: ", data);
+  
+      if(response.ok) {
+        localStorage.setItem("user", formData.Email);
+        navigate('/home');
+      }
     }
   }
 
@@ -64,13 +86,15 @@ export default function UserLogin() {
             <form action="#" className='login' onSubmit={loginHandler}>
 
               <label htmlFor="email">Email</label><br />
-              <input type="email" name="Email" id="email" required /><br />
+              <input type="email" name="Email" placeholder='example@hello.com' onChange={handleChange} required /><br />
+              {errors.Email && <span>{errors.Email}</span>}<br /><br />
 
               <label htmlFor="password">Password</label><br />
-              <input type="password" name="Password" id="password" required /><br />
+              <input type="password" name="Password" onChange={handleChange} required /><br />
+              {errors.Password && <span>{errors.Password}</span>}<br /><br />
 
-              <input type="checkbox" name="Remember" id="remember" required /><br />
-              <label htmlFor="remember">Remember Password?</label><br />
+              <input type="checkbox" name="Remember" onChange={handleChange} required />
+              <label htmlFor="remember"> Remember Password?</label><br />
 
               <input type="submit" value="Login" className='login btn' />
             </form>
