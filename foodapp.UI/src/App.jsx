5250c4b3d620008e766622f3f19e77c23ade60f2
IroshanaRavishan <bandarairoshana@gamil.com>
2024-09-14 13:55:37 +0530

    added the UserContext to provide the profile related data throughout the application

--- foodapp.UI/src/App.jsx
+++ foodapp.UI/src/App.jsx
@@ -8,6 +8,8 @@ import ProtectedRouter from "./ProtectedRouter"
 import Auth from "./components/Auth/Auth"
 import Home from "./components/Home"
 import Admin from "./components/Admin";
+import Profile from "./components/Profile";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
@@ -20,6 +22,7 @@ function App() {
         <Route element={ <ProtectedRouter /> }>
           <Route path="/home" element={ <Home foodData={foodData} setFoodData={setFoodData} setFoodId={setFoodId} foodId={foodId} /> } />
           <Route path="/admin" element={<Admin/>} />
+          <Route path="/profile" element={<Profile/>} />      
         </Route>
 
         {/* Public routes */}
@@ -43,8 +46,11 @@ function App() {
   return (
     <div className="App">
       {/* Rendering the router using RouterProvider */}
-      <Nav />
-      <RouterProvider router={router} />
+      <UserProvider > 
+      {/*This (UserProvider) ensure that all routes and components within have access to the user context. */}
+        <Nav />
+        <RouterProvider router={router} />
+      </UserProvider>
     </div>
   )
 }
--- foodapp.UI/src/ProtectedRouter.jsx
+++ foodapp.UI/src/ProtectedRouter.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { Navigate, Outlet } from 'react-router-dom';
+import { useUser } from './context/UserContext';
 
 export default function ProtectedRouter() {
-
+    const { user, setUser } = useUser(); 
     const [isLogged, setIsLogged] = useState(false);
     const [waiting, setWaiting] = useState(true);
 
@@ -11,20 +12,24 @@ export default function ProtectedRouter() {
             method: "GET",
             credentials: "include", // Include cookies in cross-origin requests
         }).then(response => {
-            if (response.ok) {
-                setIsLogged(true)
-                setWaiting(false)
+            if (response.ok) { 
+                return response.json();
+            } else {
+                throw new Error('Unauthorized');
             }
-            return response.json();
         }).then(data=> {
+            setUser(data.user);  // Store user data in context
             localStorage.setItem("user", data.user.email)
-            console.log(data.user)
+            console.log(data.user);
+            setIsLogged(true)
+            setWaiting(false)
         }).catch(err=> {
             console.log("Error protected routes: ", err);
             setWaiting(false);
             localStorage.removeItem("user");
+            setUser(null); // Clear user data in context
         })
-    },[])
+    },[setUser])
 
   return ( waiting ? <div className="waitingPage">
     <div>Waiting...</div>
