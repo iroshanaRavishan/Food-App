99fb75b9343f6b9ce9f000e5f3ba2112bbc704f0
IroshanaRavishan <bandarairoshana@gamil.com>
2024-10-01 18:04:28 +0530

    used newly created components to build the featured section

--- foodapp.UI/src/components/FeaturedSection.jsx
+++ foodapp.UI/src/components/FeaturedSection.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import styles from './featuredsection.module.css';
+import TodaysSpecialCard from './TodaysSpecialCard';
+import Promotion from './Promotion';
 
 export default function FeaturedSection() {
   return (
-        <div className={styles.greenGradient}>
+        <div className={styles.container}>
             <div className={styles.backDropImage}></div>
-            <h1>promotions</h1>
-            <div className={styles.contentContainer}>
-                <div className={styles.todaysSpecial}>
-                    <p>TODAYS SPECIAL <span>MENU</span></p>
-                </div>
-                <div className={styles.promotions}>
-                    <div className={styles.item}>Promotion goes here...</div>
-                    <div className={styles.item}>Promotion goes here...</div>
-                    <div className={styles.item}>Promotion goes here...</div>
-                    <div className={styles.item}>Promotion goes here...</div>
-                </div>
+            <div className={styles.innerContaner}>
+                <TodaysSpecialCard />
+                <Promotion />
             </div>
         </div>
     )
