97806e9866282704319a82ff65b26f470a6873ef
IroshanaRavishan <bandarairoshana@gamil.com>
2024-10-01 23:09:12 +0530

    Completed basic styles of the comp

--- foodapp.UI/src/components/HotestItem.jsx
+++ foodapp.UI/src/components/HotestItem.jsx
@@ -4,14 +4,23 @@ import styles from './hotestitem.module.css'
 export default function HotestItem() {
   return (
       <div className={styles.container}>
-          <div className={styles.content}>
-              Content goes here...
-          </div>
-          <div className={styles.mainImage}>
-              Image goes here...
-          </div>
-          <img src="./src/assets/images/juice1-1.png" alt="juice-glass"/>
-          <div className={styles.burgerImage}></div>
+            <div className={styles.content}>
+                <div className={styles.header}>
+                    <h1><span>always</span><br /> the hottest <br /> pizza</h1>
+                </div>
+                <div className={styles.description}>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id tenetur, ducimus quos earum 
+                        perferendis labore, veritatis expedita cumque.
+                    </p>
+                </div>
+                <button className={styles.getButton}>get pizza</button>
+            </div>
+            <div className={styles.mainImage}>
+                <img className={styles.pizzaBox} src="./src/assets/images/pizza-box.png" alt="pizza-box"/>
+            </div>
+            {/* <img className={styles.imageAfterContainer} src="./src/assets/images/juice1-1.png" alt="juice-glass"/> */}
+            <div className={styles.pizzaImage}></div>
       </div>
-  )
+    )
 }
