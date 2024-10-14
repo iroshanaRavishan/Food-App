befacd1c213d545f4f357057e222e9a94c096a8a
IroshanaRavishan <bandarairoshana@gamil.com>
2024-10-14 23:47:17 +0530

    added basic stucture and styles to super Deal component

--- foodapp.UI/src/components/SuperDeal.jsx
+++ foodapp.UI/src/components/SuperDeal.jsx
@@ -4,34 +4,115 @@ import styles from './superdeal.module.css'
 export default function SuperDeal() {
     return (
         <div className={styles.container}>
-            <div className={styles.header}>
-                <h1>Super Delicious Deal</h1>
-            </div>
+            <h1 className={styles.header}>Super Delicious Deal</h1>
             <div className={styles.categories}>
                 <div className={styles.categoryRow}>
                     <div className={styles.item}>
-                        item1
+                        <div className={styles.productCard}>
+                            <div className={styles.imageContainer}>
+                                <img src="./src/assets/images/cryspy-fry-burger.png" alt="Wrangler Burger" className={styles.productImage} />
+                            </div>
+                            <div className={styles.cardContent}>
+                                <h2 className={styles.productTitle}>Wrangler Burger</h2>
+                                <p className={styles.productDescription}>
+                                Blackened chicken, sauteed onions and peppers smothered with pepper jack and
+                                </p>
+                                <div className={styles.priceContainer}>
+                                <span className={styles.oldPrice}>£590.00</span>
+                                <span className={styles.newPrice}>£550.00</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className={styles.item}>
-                        item1
+                        <div className={styles.productCard}>
+                            <div className={styles.imageContainer}>
+                                <img src="./src/assets/images/fandango-burger.png" alt="Wrangler Burger" className={styles.productImage} />
+                            </div>
+                            <div className={styles.cardContent}>
+                                <h2 className={styles.productTitle}>Wrangler Burger</h2>
+                                <p className={styles.productDescription}>
+                                Blackened chicken, sauteed onions and peppers smothered with pepper jack and
+                                </p>
+                                <div className={styles.priceContainer}>
+                                <span className={styles.oldPrice}>£590.00</span>
+                                <span className={styles.newPrice}>£550.00</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className={styles.item}>
-                        item1
+                        <div className={styles.productCard}>
+                            <div className={styles.imageContainer}>
+                                <img src="./src/assets/images/wrangler-burger.png" alt="Wrangler Burger" className={styles.productImage} />
+                            </div>
+                            <div className={styles.cardContent}>
+                                <h2 className={styles.productTitle}>Wrangler Burger</h2>
+                                <p className={styles.productDescription}>
+                                Blackened chicken, sauteed onions and peppers smothered with pepper jack and
+                                </p>
+                                <div className={styles.priceContainer}>
+                                <span className={styles.oldPrice}>£590.00</span>
+                                <span className={styles.newPrice}>£550.00</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
-
                     <div className={styles.item}>
-                        item1
+                        <div className={styles.productCard}>
+                            <div className={styles.imageContainer}>
+                                <img src="./src/assets/images/double-cheese-burger.png" alt="Wrangler Burger" className={styles.productImage} />
+                            </div>
+                            <div className={styles.cardContent}>
+                                <h2 className={styles.productTitle}>Wrangler Burger</h2>
+                                <p className={styles.productDescription}>
+                                Blackened chicken, sauteed onions and peppers smothered with pepper jack and
+                                </p>
+                                <div className={styles.priceContainer}>
+                                <span className={styles.oldPrice}>£590.00</span>
+                                <span className={styles.newPrice}>£550.00</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className={styles.item}>
-                        item1
+                        <div className={styles.productCard}>
+                            <div className={styles.imageContainer}>
+                                <img src="./src/assets/images/cheddar-cheese-burger.png" alt="Wrangler Burger" className={styles.productImage} />
+                            </div>
+                            <div className={styles.cardContent}>
+                                <h2 className={styles.productTitle}>Wrangler Burger</h2>
+                                <p className={styles.productDescription}>
+                                Blackened chicken, sauteed onions and peppers smothered with pepper jack and
+                                </p>
+                                <div className={styles.priceContainer}>
+                                <span className={styles.oldPrice}>£590.00</span>
+                                <span className={styles.newPrice}>£550.00</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className={styles.item}>
-                        item1
+                        <div className={styles.productCard}>
+                            <div className={styles.imageContainer}>
+                                <img src="./src/assets/images/howdy-hamburger.png" alt="Wrangler Burger" className={styles.productImage} />
+                            </div>
+                            <div className={styles.cardContent}>
+                                <h2 className={styles.productTitle}>Wrangler Burger</h2>
+                                <p className={styles.productDescription}>
+                                Blackened chicken, sauteed onions and peppers smothered with pepper jack and
+                                </p>
+                                <div className={styles.priceContainer}>
+                                <span className={styles.oldPrice}>£590.00</span>
+                                <span className={styles.newPrice}>£550.00</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <img src="./src/assets/images/rice1-1.png" alt="rice"/>
+            <img className={styles.sideImage} src="./src/assets/images/rice1-1.png" alt="rice"/>
         </div>
     )
 }
