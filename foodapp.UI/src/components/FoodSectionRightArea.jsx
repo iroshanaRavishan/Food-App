13977be3d3758dfb41cead9fbff8f9b1f713242e
IroshanaRavishan <bandarairoshana@gamil.com>
2024-10-27 23:14:13 +0530

    added right section of the food section component

---
+++ foodapp.UI/src/components/FoodSectionRightArea.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './foodsectionrightarea.module.css';
+import DynamicDeliciousItem from './DynamicDeliciousItem';
+
+export default function FoodSectionRightArea() {
+  return (
+    <div className={styles.container}>
+        <DynamicDeliciousItem />
+    </div>
+  )
+}
