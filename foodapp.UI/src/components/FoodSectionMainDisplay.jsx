ddd7a92d5a646f64ca6bcd4176e6661c19792c79
IroshanaRavishan <bandarairoshana@gamil.com>
2024-11-13 20:53:26 +0530

    use the sorting option in the main display of the food section component

--- foodapp.UI/src/components/FoodSectionMainDisplay.jsx
+++ foodapp.UI/src/components/FoodSectionMainDisplay.jsx
@@ -2,15 +2,23 @@ import React, { useState } from 'react';
 import styles from './foodsectionmaindisplay.module.css';
 import CategoryScroller from './CategoryScroller';
 import MainDisplayFoodItem from './MainDisplayFoodItem';
+import SortResult from './SortResult';
 
 export default function FoodSectionMainDisplay({ sectionName }) {
-  const [selectedItem, setSelectedItem] = useState('burgers');
+  const [selectedCategory, setSelectedCategory] = useState('burgers');
 
   return (
     <div className={styles.container}>
         <h3 className={styles.heading}>Categories</h3>
-        <CategoryScroller setSelectedItem={setSelectedItem} selectedItem={selectedItem} />
-        <h3 className={styles.subHeading}>{selectedItem}</h3>
+        <CategoryScroller setSelectedCategory={setSelectedCategory} selectedCategory={selectedCategory} />
+
+        <div className={styles.foodHeadingArea}>
+          <div className={styles.subHeaderContainer}>
+            <h3 className={styles.subHeading}>{selectedCategory}</h3>
+          </div>
+          <SortResult />
+        </div>
+
         <MainDisplayFoodItem />
     </div>
   )
